bb452a142f5df9edaac7e41eedb025a5c6487d3a
aiken <1961641435@qq.com>
2024-07-09 10:19:51 +0800

    部署测试

--- src/app/api/saveDialog/route.ts
+++ src/app/api/saveDialog/route.ts
@@ -3,40 +3,51 @@ import { db } from '@/server/db'
 import { chats } from '@/server/db/schema'
 import { getUserId } from '@/utils/getUserId'
 
-async function streamToArrayBuffer(stream:ReadableStream) {
-  const reader = stream.getReader();
-  const chunks = [];
-  let done = false;
-
+async function streamToArrayBuffer(stream: ReadableStream) {
+  const reader = stream.getReader()
+  const chunks = []
+  let done = false
   while (!done) {
-    const { value, done: readerDone } = await reader.read();
-    done = readerDone;
+    const { value, done: readerDone } = await reader.read()
+    done = readerDone
     if (value) {
-      chunks.push(value);
+      chunks.push(value)
     }
   }
-
-  const arrayBuffer = new Uint8Array(chunks.reduce((acc, val) => acc.concat(Array.from(val)), [])).buffer;
-  return arrayBuffer;
+  const arrayBuffer = new Uint8Array(
+    chunks.reduce((acc, val) => acc.concat(Array.from(val)), [])
+  ).buffer
+  return arrayBuffer
 }
 
-// async function readJSONBody(req: NextRequest) {
-//   const chunks = []
-//   for await (const chunk of req.body) {
-//     chunks.push(chunk)
-//   }
-//   const rawBody = Buffer.concat(chunks).toString('utf-8')
-//   return JSON.parse(rawBody)
-// }
+export async function POST(request: NextRequest) {
+  const userId = await getUserId()
+  
+  if(!request.body){
+    return NextResponse.json({message: 'empty!'})
+  }
+  const arrayBuffer = await streamToArrayBuffer(request.body as ReadableStream)
+  const text = new TextDecoder('utf-8').decode(arrayBuffer)
+  const body: {botId:string, timestamp:number, dialog:Record<string,any>} = JSON.parse(text)
+  const { botId, timestamp, dialog } = body
 
+  await db
+    .insert(chats)
+    .values({ userId, botId, timestamp, dialog: JSON.stringify(dialog) })
+  return NextResponse.json({ message: 'save dialog success!' })
+}
+
+/**
 export async function POST(request: NextRequest) {
   const userId = await getUserId()
   // console.log(
   //   '------saveDialog request------',
   //   request.body instanceof ReadableStream
   // )
-  // const body = await readJSONBody(request)
-  const arrayBuffer = await streamToArrayBuffer(request.body as ReadableStream<Uint8Array>);
+  if(!request.body){
+    return NextResponse.json({message: 'empty!'})
+  }
+  const arrayBuffer = await streamToArrayBuffer(request.body as ReadableStream);
   const text = new TextDecoder('utf-8').decode(arrayBuffer);
   const body = JSON.parse(text);
   const { botId, timestamp, dialog } = body
@@ -52,3 +63,4 @@ export async function POST(request: NextRequest) {
     .values({ userId, botId, timestamp, dialog: JSON.stringify(dialog) })
   return NextResponse.json({ message: 'save dialog success!' })
 }
+ */
