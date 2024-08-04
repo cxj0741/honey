0b43e31043e4b98d2f5fe910c48141e4f0b7deef
liziheng <2792685313@qq.com>
2024-08-04 09:57:54 +0800

    bug修改-部署

--- src/components/web/LoginDialog.tsx
+++ src/components/web/LoginDialog.tsx
@@ -1,6 +1,6 @@
 import { signUp } from '@/request'
 import { signIn } from 'next-auth/react'
-import { useRef, useState } from 'react'
+import { useRef } from 'react'
 import { z } from 'zod'
 import Toast, { TOAST_TYPE, useToast } from './Toast'
 import { useRouter } from 'next/navigation'
@@ -129,7 +129,7 @@ export default function LoginDialog({ type, setType, dialogShow, setDialogShow }
                   >
                     <path
                       fillRule="evenodd"
-                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 1 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                       clipRule="evenodd"
                     />
                   </svg>
